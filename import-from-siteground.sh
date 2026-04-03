fd789a831f92a756ba8b18420274570a993b0ca8
xiaohye910-maker <xiaohye910@gmail.com>
2026-04-02 21:26:48 -0400

    Initial import from SiteGround (jnbhealth.com)

---
+++ import-from-siteground.sh
@@ -0,0 +1,20 @@
+﻿#!/usr/bin/env bash
+set -euo pipefail
+
+SSH_HOST="ssh.jnbhealth.com"
+SSH_USER="u1050-0yzufsdczrls"
+SSH_PORT="18765"
+REMOTE_PATH="/home/u1050-0yzufsdczrls/public_html/"
+LOCAL_DEST="$(pwd)"
+
+echo "==> Syncing files from SiteGround..."
+rsync -avz --progress \
+  -e "ssh -p $SSH_PORT -o StrictHostKeyChecking=no" \
+  --exclude="wp-content/upgrade/" \
+  --exclude="wp-content/backup-db/" \
+  --exclude="wp-content/cache/" \
+  --exclude="wp-content/w3tc-cache/" \
+  --exclude=".git/" \
+  --exclude="*.log" \
+  "${SSH_USER}@${SSH_HOST}:${REMOTE_PATH}" \
+  "$LOCAL_DEST/"
